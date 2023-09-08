3fd486eaf524c1aec886da0983ebf8558008be94
Lillian Lecca Rafael <leleccar@ncsu.edu>
2023-09-07 21:51:34 -0400

    Fall: test

--- Fall/tictactoe.py
+++ Fall/tictactoe.py
@@ -142,7 +142,7 @@ class TicTacToeGame:
         if winner:
             print(f"Player {winner} wins!")
         else:
-            print("It's a tie!")
+            print("It's a tie! 2020")
 
 
 TicTacToe = TicTacToeGame()
